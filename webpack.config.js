25231336dac0494f7ddb706a29e2645f90d734c4
Kent McKinney <kent.p.mckinney@gmail.com>
2020-02-25 18:01:10 -0800

    add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-var webpack = require("webpack");
 
 module.exports = {
   entry: './src/main.js',
@@ -22,11 +21,6 @@ module.exports = {
       template: './src/index.html',
       inject: 'body'
     })
-  //   ,
-  //   new webpack.ProvidePlugin({
-  //     $: "jquery",
-  //     jQuery: "jquery"
-  // })
   ],
   module: {
     rules: [
